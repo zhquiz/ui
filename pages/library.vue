c46ff4097251a2ea412aa5eb87d362d75b37d9b0
Pacharapol Withayasakpunt <polv@polv.cc>
2021-01-07 20:50:29 +0700

    prepare for library

--- pages/library.vue
+++ pages/library.vue
@@ -1,14 +1,14 @@
 <template>
   <section>
     <div class="LibraryPage">
-      <h2 class="title is-2">HSK</h2>
+      <h2 class="title is-2">Library</h2>
       <b-collapse
-        v-for="(collapse, index) of collapses"
-        :key="index"
+        v-for="(c, i) of collapses"
+        :key="i"
         class="card"
         animation="slide"
-        :open="openIndex == index"
-        @open="openIndex = index"
+        :open="cIndex == i"
+        @open="cIndex = i"
       >
         <div
           slot="trigger"
@@ -25,29 +25,62 @@
               }
             "
           >
-            {{ collapse.title }}
+            {{ c.title }}
           </p>
           <a class="card-header-icon">
             <b-icon :icon="props.open ? 'caret-down' : 'caret-up'"> </b-icon>
           </a>
         </div>
-        <div class="card-content">
-          <div class="content">
-            <span
-              v-for="t in currentData"
-              :key="t"
-              class="tag clickable"
-              :class="getTagClass(t)"
-              @contextmenu.prevent="
-                (evt) => {
-                  selected = [t]
-                  $refs.context.open(evt)
-                }
-              "
+        <div class="card-content top-card-content">
+          <b-collapse
+            v-for="(c0, j) of c.children"
+            :key="j"
+            class="card"
+            animation="slide"
+            :open="cSubIndex == j"
+            @open="cSubIndex = j"
+          >
+            <div
+              slot="trigger"
+              slot-scope="props"
+              class="card-header"
+              role="button"
             >
-              {{ t }}
-            </span>
-          </div>
+              <p
+                class="card-header-title"
+                @contextmenu.prevent="
+                  (evt) => {
+                    selected = currentData
+                    $refs.context.open(evt)
+                  }
+                "
+              >
+                {{ c0.title }}
+              </p>
+              <a class="card-header-icon">
+                <b-icon :icon="props.open ? 'caret-down' : 'caret-up'">
+                </b-icon>
+              </a>
+            </div>
+            <div class="card-content">
+              <div>
+                <span
+                  v-for="t in currentData"
+                  :key="t"
+                  class="tag clickable"
+                  :class="getTagClass(t)"
+                  @contextmenu.prevent="
+                    (evt) => {
+                      selected = [t]
+                      $refs.context.open(evt)
+                    }
+                  "
+                >
+                  {{ t }}
+                </span>
+              </div>
+            </div>
+          </b-collapse>
         </div>
       </b-collapse>
     </div>
@@ -81,7 +114,6 @@ import ContextMenu from '~/components/ContextMenu.vue'
 export default class LibraryPage extends Vue {
   @Ref() context!: ContextMenu
 
-  openIndex = 0
   selected: string[] = []
   srsLevel: {
     [entry: string]: number
@@ -93,74 +125,82 @@ export default class LibraryPage extends Vue {
     (lv: any) => (lv === 0 ? 'is-danger' : ''),
   ]
 
-  readonly collapses = [
+  cIndex = 0
+  cSubIndex = -1
+
+  collapses = [
     {
-      title: 'HSK1',
-      q: 'tag:HSK1',
-    },
-    {
-      title: 'HSK2',
-      q: 'tag:HSK2',
-    },
-    {
-      title: 'HSK3',
-      q: 'tag:HSK3',
-    },
-    {
-      title: 'HSK4 (Set 1)',
-      q: 'tag:HSK4 tag:HSK4_set1',
-    },
-    {
-      title: 'HSK4 (Set 2)',
-      q: 'tag:HSK4 NOT tag:HSK4_set1',
-    },
-    {
-      title: 'HSK5 (Set 1)',
-      q: 'tag:HSK5 tag:HSK5_set1',
-    },
-    {
-      title: 'HSK5 (Set 2)',
-      q: 'tag:HSK5 tag:HSK5_set2',
-    },
-    {
-      title: 'HSK5 (Set 3)',
-      q: 'tag:HSK5 tag:HSK5_set3',
-    },
-    {
-      title: 'HSK5 (Set 4)',
-      q: 'tag:HSK5 NOT (tag:HSK5_set1 OR tag:HSK5_set2 OR tag:HSK5_set3)',
-    },
-    {
-      title: 'HSK6 (Set 1)',
-      q: 'tag:HSK6 tag:HSK6_set1',
-    },
-    {
-      title: 'HSK6 (Set 2)',
-      q: 'tag:HSK6 tag:HSK6_set2',
-    },
-    {
-      title: 'HSK6 (Set 3)',
-      q: 'tag:HSK6 tag:HSK6_set3',
-    },
-    {
-      title: 'HSK6 (Set 4)',
-      q: 'tag:HSK6 tag:HSK6_set4',
-    },
-    {
-      title: 'HSK6 (Set 5)',
-      q:
-        'tag:HSK6 NOT (tag:HSK6_set1 OR tag:HSK6_set2 OR tag:HSK6_set3 OR tag:HSK6_set4)',
+      title: 'HSK',
+      children: [
+        {
+          title: 'HSK1',
+          q: 'tag:HSK1',
+        },
+        {
+          title: 'HSK2',
+          q: 'tag:HSK2',
+        },
+        {
+          title: 'HSK3',
+          q: 'tag:HSK3',
+        },
+        {
+          title: 'HSK4 (Set 1)',
+          q: 'tag:HSK4 tag:HSK4_set1',
+        },
+        {
+          title: 'HSK4 (Set 2)',
+          q: 'tag:HSK4 NOT tag:HSK4_set1',
+        },
+        {
+          title: 'HSK5 (Set 1)',
+          q: 'tag:HSK5 tag:HSK5_set1',
+        },
+        {
+          title: 'HSK5 (Set 2)',
+          q: 'tag:HSK5 tag:HSK5_set2',
+        },
+        {
+          title: 'HSK5 (Set 3)',
+          q: 'tag:HSK5 tag:HSK5_set3',
+        },
+        {
+          title: 'HSK5 (Set 4)',
+          q: 'tag:HSK5 NOT (tag:HSK5_set1 OR tag:HSK5_set2 OR tag:HSK5_set3)',
+        },
+        {
+          title: 'HSK6 (Set 1)',
+          q: 'tag:HSK6 tag:HSK6_set1',
+        },
+        {
+          title: 'HSK6 (Set 2)',
+          q: 'tag:HSK6 tag:HSK6_set2',
+        },
+        {
+          title: 'HSK6 (Set 3)',
+          q: 'tag:HSK6 tag:HSK6_set3',
+        },
+        {
+          title: 'HSK6 (Set 4)',
+          q: 'tag:HSK6 tag:HSK6_set4',
+        },
+        {
+          title: 'HSK6 (Set 5)',
+          q:
+            'tag:HSK6 NOT (tag:HSK6_set1 OR tag:HSK6_set2 OR tag:HSK6_set3 OR tag:HSK6_set4)',
+        },
+      ],
     },
   ]
 
   currentData: string[] = []
 
   get q() {
-    return this.collapses[this.openIndex].q
+    return this.collapses[this.cIndex]?.children[this.cSubIndex]?.q || ''
   }
 
   async reload(entries: string[]) {
-    if (this.currentData.length === 0) {
+    if (this.currentData.length === 0 && this.q) {
       const { result } = await this.$axios.$get<{
         result: {
           entry: string
@@ -228,5 +268,10 @@ export default class LibraryPage extends Vue {
 <style scoped>
 .tag {
   margin-right: 0.5rem;
+}
+
+.top-card-content {
+  max-height: 400px;
+  overflow: scroll;
 }
 </style>

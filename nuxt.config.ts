658e7cc0513dcb11fa6a4e878215973797f0dd11
Pacharapol Withayasakpunt <polv@polv.cc>
2021-01-11 00:51:32 +0700

    plan electron server

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -32,7 +32,26 @@ export default (): NuxtConfig => {
           content: 'chinese,mandarin,srs,quiz',
         },
       ],
-      link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+      link: [
+        {
+          rel: 'apple-touch-icon',
+          sizes: '180x180',
+          href: '/apple-touch-icon.png',
+        },
+        {
+          rel: 'icon',
+          type: 'image/png',
+          sizes: '32x32',
+          href: '/favicon-32x32.png',
+        },
+        {
+          rel: 'icon',
+          type: 'image/png',
+          sizes: '16x16',
+          href: '/favicon-16x16.png',
+        },
+        { rel: 'manifest', type: 'image/x-icon', href: '/site.manifest' },
+      ],
     },
     /*
      ** Global CSS
@@ -139,7 +158,7 @@ export default (): NuxtConfig => {
       transpile: [/typed-vuex/],
     },
     generate: {
-      dir: '../go-zhquiz/public',
+      dir: process.env.OUTDIR || '../go-zhquiz/public',
     },
   }
 }
---
+++ pages/loading.vue
@@ -0,0 +1,3 @@
+<template>
+  <b-loading is-full-page></b-loading>
+</template>
